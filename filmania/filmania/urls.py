f580e0323c7a31aa8872cfee69f76a1640531b44
J4KV8 <254481@student.pwr.edu.pl>
2021-12-17 15:44:17 +0100

    Strona główna (w tworzeniu)
    Wrzucam zalążek strony głównej. W weekend postaram się zrobić więcej

--- filmania/filmania/urls.py
+++ filmania/filmania/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from pages.views import Index
 
 urlpatterns = [
+    path('', Index.as_view(), name='index'),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls'))
 ]
