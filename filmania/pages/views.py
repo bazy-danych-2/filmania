8c4e49ac5a1521dc9fa40e09b20357fc139ea104
metalic24 <65673563+metalic24@users.noreply.github.com>
2022-01-13 19:32:15 +0100

    główna strona widać nasze filmy

--- filmania/pages/views.py
+++ filmania/pages/views.py
@@ -1,10 +1,21 @@
+from typing import ContextManager
 from django.shortcuts import render
 from django.views import View
+from movies.models import Movie
 
 # Create your views here.
 class Index(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'pages/index.html')
+
+        movies = Movie.objects.filter(ratings__isnull=False).order_by('ratings__average')[:6]
+
+       
+        context = {
+
+            'movies': movies
+        }
+
+        return render(request, 'pages/index.html', context)
 
 
 
