521b085a98f8636ca9734bb06a0352e6aad9640b
metalic24 <65673563+metalic24@users.noreply.github.com>
2022-01-20 19:15:06 +0100

    aaa

--- filmania/accounts/models.py
+++ filmania/accounts/models.py
@@ -2,13 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.deletion import CASCADE
 from django.db.models.fields.related import ForeignKey, ManyToManyField, OneToOneField
+from django.forms import BooleanField
 from movies.models import Movie
 
 # Create your models here.
 
 class User_info(models.Model):
    user =  OneToOneField(User, on_delete=CASCADE)
-   ulubione_filmy = ManyToManyField(Movie, blank=True, null=True)
-
+   ulubione_filmy = ManyToManyField(Movie, blank=True)
+   is_autor = BooleanField()
    def __str__(self):
        return f"{self.user.username} - {self.pk}"
