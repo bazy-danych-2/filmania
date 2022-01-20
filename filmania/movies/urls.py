885e4e8492ccf1638bc80ee39c446299a36f18ec
metalic24 <65673563+metalic24@users.noreply.github.com>
2022-01-20 20:18:28 +0100

    usuwanie filmów działa

--- filmania/movies/urls.py
+++ filmania/movies/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('create/',views.create_movie, name='create-movie'),
     path('update/<int:id>/',views.update_movie, name='update-movie'),
+    path('delete/<int:id>/',views.delete_movie, name='delete-movie'),
     path('',views.show_all_movies, name='show_all'),
     path('details/<int:id>/',views.movie_details, name='details-movie'),
     
